59c8b6718199111148c30e8d6a04632610bb06a4
이선오 <dosori102@gmail.com>
2020-10-07 23:19:49 +0900

    Merge commit '2e869b264b6b27df3183d92e505529eadda3f125' into seono

--- app/src/main/java/com/example/barcodereader/MainActivity.java
+++ app/src/main/java/com/example/barcodereader/MainActivity.java
@@ -1,60 +1,71 @@
 package com.example.barcodereader;
 
+import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
+import com.google.zxing.ResultPoint;
+import com.journeyapps.barcodescanner.BarcodeCallback;
+import com.journeyapps.barcodescanner.BarcodeResult;
+import com.journeyapps.barcodescanner.CaptureManager;
+import com.journeyapps.barcodescanner.DecoratedBarcodeView;
+
 import android.widget.Toast;
-
-import com.google.zxing.integration.android.IntentIntegrator;
-import com.google.zxing.integration.android.IntentResult;
-
-import android.os.Bundle;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    private CaptureManager capture;
+    private DecoratedBarcodeView BarcodeView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        new IntentIntegrator(this).initiateScan();
+
+        BarcodeView = (DecoratedBarcodeView) findViewById(R.id.barcode_scanner);
+
+        capture = new CaptureManager(this, BarcodeView);
+        capture.initializeFromIntent(this.getIntent(), savedInstanceState);
+        capture.decode();
+
+        BarcodeView.decodeContinuous(new BarcodeCallback() {
+            @Override
+            public void barcodeResult(BarcodeResult result) {
+                makeText(result.toString());
+            }
+
+            @Override
+            public void possibleResultPoints(List<ResultPoint> resultPoints) {
+            }
+        });
     }
 
-//
-//    @Override
-//    protected void onResume() {
-//        super.onResume();
-//        capture.onResume();
-//    }
-//
-//    @Override
-//    protected void onPause() {
-//        super.onPause();
-//        capture.onPause();
-//    }
-//
-//    @Override
-//    protected void onDestroy() {
-//        super.onDestroy();
-//        capture.onDestroy();
-//    }
-//
-//    @Override
-//    protected void onSaveInstanceState(Bundle outState) {
-//        super.onSaveInstanceState(outState);
-//        capture.onSaveInstanceState(outState);
-//    }
-//
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        IntentResult result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data);
-        if (result != null) {
-            if (result.getContents() == null) {
-                Toast.makeText(this, "Cancelled", Toast.LENGTH_LONG).show();
-            } else {
-                Toast.makeText(this, "Scanned: " + result.getContents(), Toast.LENGTH_LONG).show();
-            }
-        } else {
-            super.onActivityResult(requestCode, resultCode, data);
-        }
+    public void makeText(String result) {
+        Toast.makeText(this, "바코드 숫자: " + result, Toast.LENGTH_SHORT).show();
     }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        capture.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        capture.onPause();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        capture.onDestroy();
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        capture.onSaveInstanceState(outState);
+    }
+
 }
