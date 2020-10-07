2e869b264b6b27df3183d92e505529eadda3f125
이선오 <dosori102@gmail.com>
2020-10-07 23:02:45 +0900

    basic UI

--- app/src/main/java/com/example/barcodereader/MainActivity.java
+++ app/src/main/java/com/example/barcodereader/MainActivity.java
@@ -2,6 +2,12 @@ package com.example.barcodereader;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.widget.Toast;
+
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
+
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
@@ -10,5 +16,45 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        new IntentIntegrator(this).initiateScan();
+    }
+
+//
+//    @Override
+//    protected void onResume() {
+//        super.onResume();
+//        capture.onResume();
+//    }
+//
+//    @Override
+//    protected void onPause() {
+//        super.onPause();
+//        capture.onPause();
+//    }
+//
+//    @Override
+//    protected void onDestroy() {
+//        super.onDestroy();
+//        capture.onDestroy();
+//    }
+//
+//    @Override
+//    protected void onSaveInstanceState(Bundle outState) {
+//        super.onSaveInstanceState(outState);
+//        capture.onSaveInstanceState(outState);
+//    }
+//
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        IntentResult result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data);
+        if (result != null) {
+            if (result.getContents() == null) {
+                Toast.makeText(this, "Cancelled", Toast.LENGTH_LONG).show();
+            } else {
+                Toast.makeText(this, "Scanned: " + result.getContents(), Toast.LENGTH_LONG).show();
+            }
+        } else {
+            super.onActivityResult(requestCode, resultCode, data);
+        }
     }
 }
